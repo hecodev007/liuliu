64d856e8f58b65a35418a63c32b895621ed2d2bf
tyz518518 <920993517@qq.com>
2022-01-28 10:44:46 +0800

    dd

--- run_both.js
+++ run_both.js
@@ -1,6 +1,8 @@
 const Web3 = require('web3');
 const fs = require('fs');
 const web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545/');
+//const web3 = new Web3('https://bsc-dataseed.binance.org/');
+
 let path = __dirname + '/keys.json';
 const helper = require('./helper');
 
